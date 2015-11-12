ec39acbb3c33bc4cfeb862cf5c20c57b6dcda864
Michael Cole <michaelcole1991@gmail.com>
2015-11-12 17:14:17 +0000

    Initial admin changes
    adding in acl, menu, admin routes and grid controller

---
+++ Controller/Adminhtml/Post/Index.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Mc\Blog\Controller\Adminhtml\Post;
+
+use Magento\Backend\App\Action\Context;
+use Magento\Framework\View\Result\PageFactory;
+
+class Index extends \Magento\Backend\App\Action
+{
+    const ADMIN_RESOURCE = 'Mc_Blog::post';
+
+    protected $resultPageFactory;
+
+    public function __construct (
+        Context $context,
+        PageFactory $resultPageFactory
+    ) {
+        parent::__construct($context);
+        $this->resultPageFactory = $resultPageFactory;
+    }
+
+    public function execute()
+    {
+        var_dump('posts index');
+        $resultPage = $this->resultPageFactory->create();
+        $resultPage->setActiveMenu('Mc_Blog::post');
+        $resultPage->addBreadCrumb(__('Blog Posts'), __('Blog Posts'));
+        $resultPage->addBreadCrumb(__('Manage Blog Posts'), __('Manage Blog Posts'));
+        $resultPage->getConfig()->getTitle()->prepend(__('Blog Posts'));
+
+        return $resultPage;
+    }
+}
